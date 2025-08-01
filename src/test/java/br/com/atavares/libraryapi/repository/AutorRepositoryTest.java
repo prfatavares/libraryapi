037768b969a1e057514795a636d3c8ca503746e6
Alexandre Tavares <alexandre@atavares.com.br>
2025-07-31 22:28:56 -0300

    Incluída classe de teste AutorRepositoryTest

---
+++ src/test/java/br/com/atavares/libraryapi/repository/AutorRepositoryTest.java
@@ -0,0 +1,25 @@
+package br.com.atavares.libraryapi.repository;
+
+import br.com.atavares.libraryapi.model.Autor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+
+@SpringBootTest
+public class AutorRepositoryTest {
+
+    @Autowired
+    private AutorRepository autorRepository;
+
+    @Test
+    public void salvarTest(){
+        Autor autor = new Autor();
+        autor.setNome("Carlos Drummond de Andrade");
+        autor.setNacionalidade("Brasileira");
+        autor.setDataNascimento(LocalDate.of(1972, 12, 23));
+        autorRepository.save(autor);
+        System.out.println("Autor salvo com sucesso: " + autor);
+    }
+}
