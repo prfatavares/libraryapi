cb8d9b105e113dfb0f22b8626eab7d64e5885425
Alexandre Tavares <alexandre@atavares.com.br>
2025-08-01 23:28:16 -0300

    Inclusão de Query Methods

--- src/main/java/br/com/atavares/libraryapi/repository/LivroRepository.java
+++ src/main/java/br/com/atavares/libraryapi/repository/LivroRepository.java
@@ -1,9 +1,16 @@
 package br.com.atavares.libraryapi.repository;
 
+import br.com.atavares.libraryapi.model.GeneroLivro;
 import br.com.atavares.libraryapi.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.UUID;
 
 public interface LivroRepository extends JpaRepository<Livro, UUID> {
+
+    Livro findByTitulo(String titulo);
+    Livro findByIsbnAndId(String isbn, UUID id);
+    List<Livro> findByPrecoOrGenero(BigDecimal preco, GeneroLivro genero);
 }
--- src/test/java/br/com/atavares/libraryapi/repository/LivroRepositoryTest.java
+++ src/test/java/br/com/atavares/libraryapi/repository/LivroRepositoryTest.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @SpringBootTest
@@ -19,6 +21,8 @@ class LivroRepositoryTest {
 
     @Autowired
     private AutorRepository autorRepository;
+
+    List<Livro> livros = new ArrayList<>();
 
     @Test
     public void salvarTest(){
@@ -53,6 +57,15 @@ class LivroRepositoryTest {
 
         livro.setAutor(autor);
         livroRepository.save(livro);
+    }
+
+    @Test
+    public void listar(){
+        System.out.println("\nLivro por título:\n" + livroRepository.findByTitulo("Poesias"));
+        System.out.println("\nLivro por ISBN e ID:\n" + livroRepository.findByIsbnAndId("987654321", UUID.fromString("a9eb697b-a21e-4d63-9fe3-d331dfba2081")));
+        livros = livroRepository.findByPrecoOrGenero(BigDecimal.valueOf(89.90), GeneroLivro.FICCAO);
+        System.out.println("\nLivros por preço ou Genero:");
+        livros.forEach(System.out::println);
     }
 
 }
